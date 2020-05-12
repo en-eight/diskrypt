136a6903adbbb41d7a3b647df6e1397c3437fedb
Nate Holcomb <sadhorseman@pm.me>
2020-05-12 00:12:38 -0400

    Finished main command line impl

---
+++ Diskrypt.py
@@ -0,0 +1,36 @@
+import hashlib
+from enum import Enum
+from itertools import cycle
+import sys
+import base64
+
+def encryptStr(userString, userKey):
+	return "".join(chr(ord(x) ^ ord(y)) for x,y in zip(userString,userKey))
+
+
+class ERROR(Enum):
+	ARGV_EXCEED = 1
+	FILE_INVALID = 2
+	FILE_READ_ERROR = 3
+	FILE_WRITE_ERROR = 4
+
+if len(sys.argv) > 3:
+	print('''\n\nError! Too many arguments.
+Usage: python diskryptEncrypt.py filename.dskp decryption_key\n\n''')
+	sys.exit(ERROR.ARGV_EXCEED)
+
+#Assigning variables from the argument vector.
+userFile = sys.argv[1]
+userKey = sys.argv[2]
+
+if ".dskp" in userFile or ".txt" in userFile:
+	with open(userFile, 'r+', encoding="utf-16") as f:
+		data = f.read()
+		f.seek(0)
+		for l in data:
+			f.write(encryptStr(l, userKey))
+
+else:
+	print('''\n\nError! Invalid file extension.
+Accepted file extensions: .dskp .txt\n\n''')
+	sys.exit(ERROR.FILE_INVALID)
--- diskryptEncrypt.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-from enum import Enum
-import sys
-
-if len(sys.argv) > 3:
-	print('''\n\nError! Too many arguments.
-Usage: python diskryptEncrypt.py filename.dskt decryption_key\n\n''')
-	sys.exit(0)
