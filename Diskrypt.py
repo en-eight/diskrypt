b7bd1fe2e8c6f087ea368cbb2c2bfe98d2ad07de
Nate Holcomb <sadhorseman@pm.me>
2020-08-08 11:10:06 -0400

    Improved code

--- Diskrypt.py
+++ Diskrypt.py
@@ -16,23 +16,31 @@ class ERROR(Enum):
 	FILE_WRIT_ERROR = 4
 	FILE_DNE = 5
 
+#error check - argv count
 if len(sys.argv) != 3:
+	print('\n\nUnsuccessful run; the file was not altered. See below:')
 	print('''\n\nError! Invalid # of arguments.
-Usage: python diskryptEncrypt.py filename.dskp decryption_key\n\n''')
+	Usage: python ./Diskrypt.py filename.dskp decryption_key\n\n''')
 	sys.exit(ERROR.ARGV_NEQUAL)
 
 #Assigning variables from the argument vector.
 userFile = sys.argv[1]
 userKey = sys.argv[2]
 
+#some part in this process creates artifacts...
 if ".dskp" in userFile or ".txt" in userFile and os.path.isfile(userFile) == True:
 	with open(userFile, 'r+', encoding="utf-16") as f:
 		data = f.read()
 		f.seek(0)
+		#I'm thinking it's here.
 		for l in data:
 			f.write(encryptStr(l, userKey))
+	f.close
+	print("\nSuccessful run. File was altered.\n")
+	sys.exit()
 
 else:
+	print('\n\nUnsuccessful run; the file was not altered. See below:')
 	print('''\n\nError! Invalid file extension.
 Accepted file extensions: .dskp .txt\n\n''')
 	sys.exit(ERROR.FILE_INVALID)
